66888802758c922aeeb52622c6faebb619aea8e5
federicoghedinastailamedia <federico.ghedina@stailamedia.com>
2016-11-17 14:48:51 +0100

    better error handling

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ function malta_coffeescript(o, options) {
 		start = new Date(),
 		msg,
 		dir = path.dirname(o.name),
-		oldname = o.name;
+		oldname = o.name,
+		pluginName = path.basename(path.dirname(__filename)),
+		doErr = function (e) {
+			console.log(('[ERROR on ' + o.name + ' using ' + pluginName + '] :').red());
+			console.dir(e);
+			self.stop();
+		};
 
 	return function (solve, reject){
-		var ls = child_process.spawn('coffee', ['--compile', o.name]);
-		
-		ls.on('exit', function (code) {
-			o.name = o.name.replace(/\.coffee$/, '.js');
-			o.content = fs.readFileSync(o.name) + "";
-			msg = 'plugin ' + path.basename(path.dirname(__filename)).white() + ' wrote ' + o.name;
-			fs.unlink(oldname);
-			solve(o);
-			self.notifyAndUnlock(start, msg);
-		});
-
-		ls.stdout.on('data', function(data) {
-			self.log_debug(data + "");
-		});
-
-		ls.stderr.on('error', function (data) {
-			self.log_err('stderr: ' + data);
-		});
-		ls.exit();
+		try {
+			var ls = child_process.spawn('coffee', ['--compile', o.name]);
+			ls.on('exit', function (code) {
+				o.name = o.name.replace(/\.coffee$/, '.js');
+				o.content = fs.readFileSync(o.name) + "";
+				msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name;
+				fs.unlink(oldname);
+				solve(o);
+				self.notifyAndUnlock(start, msg);
+			});
+		} catch (err) {
+			doErr(err);
+		}
 	};
 }
 malta_coffeescript.ext = 'coffee';
