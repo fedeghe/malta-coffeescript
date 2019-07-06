dd7c92b8ad15daba821eb290a21dc829f533c24d
fedeghe <fedeghe@gmail.com>
2019-07-06 22:51:45 +0200

    reject set

--- index.js
+++ index.js
@@ -5,33 +5,34 @@ require('malta').checkExec('coffee');
 // npm install -g coffee-script
 //
 var path = require('path'),
-	fs = require('fs'),
-	child_process = require('child_process');
+    fs = require('fs'),
+    child_process = require('child_process');
 
 function malta_coffeescript(o, options) {
 
-	var self = this,
-		start = new Date(),
-		msg,
-		dir = path.dirname(o.name),
-		oldname = o.name,
-		pluginName = path.basename(path.dirname(__filename));
+    var self = this,
+        start = new Date(),
+        msg,
+        dir = path.dirname(o.name),
+        oldname = o.name,
+        pluginName = path.basename(path.dirname(__filename));
 
-	return function (solve, reject){
-		try {
-			var ls = child_process.spawn('coffee', ['--compile', o.name]);
-			ls.on('exit', function (code) {
-				o.name = o.name.replace(/\.coffee$/, '.js');
-				o.content = fs.readFileSync(o.name) + "";
-				msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name;
-				fs.unlink(oldname, () => {});
-				solve(o);
-				self.notifyAndUnlock(start, msg);
-			});
-		} catch (err) {
-			self.doErr(err, o, pluginName);
-		}
-	};
+    return function (solve, reject) {
+        try {
+            var ls = child_process.spawn('coffee', ['--compile', o.name]);
+            ls.on('exit', function (code) {
+                o.name = o.name.replace(/\.coffee$/, '.js');
+                o.content = fs.readFileSync(o.name) + "";
+                msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name;
+                fs.unlink(oldname, () => { });
+                solve(o);
+                self.notifyAndUnlock(start, msg);
+            });
+        } catch (err) {
+            reject(`Plugin ${pluginName} compilation error:\n${err}`);
+            self.doErr(err, o, pluginName);
+        }
+    };
 }
 malta_coffeescript.ext = 'coffee';
 module.exports = malta_coffeescript;
